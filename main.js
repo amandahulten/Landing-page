848dfa9fb56477ec407cf160c0b512011c9af606
Marcus Hägerstrand <marcus.hagerstrand@gmail.com>
2021-11-19 11:54:50 +0100

    applied sticky to nav

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
+// White nav bg on scroll
+
+window.onscroll = () => {
+  const nav = document.querySelector('nav');
+    if(this.scrollY <= 40) {
+      nav.className = '';
+    } else {
+      nav.className = 'scroll';
+    }
+};
+
+// Marquee text effect
+
 Marquee3k.init({
 	selector: 'marquee',
 });
