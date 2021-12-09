b4b2bae4685b9eb635c21d19cd0f43aa7e5bbe9b
amandahulten <amahul1029@skola.goteborg.se>
2021-12-09 12:44:25 +0100

    added responsive navbar

--- main.js
+++ main.js
@@ -2,18 +2,28 @@
 
 window.onscroll = () => {
   const nav = document.querySelector('nav');
-    if(this.scrollY <= 40) {
-      nav.className = '';
-    } else {
-      nav.className = 'scroll';
-    }
+  if (this.scrollY <= 40) {
+    nav.className = '';
+  } else {
+    nav.className = 'scroll';
+  }
 };
 
 // Marquee text effect
 
-Marquee3k.init({
-	selector: 'marquee',
-});
-window.addEventListener('load', (event) => {
-   Marquee3k.refreshAll();
-});
+// Marquee3k.init({
+//   selector: 'marquee',
+// });
+// window.addEventListener('load', (event) => {
+//   Marquee3k.refreshAll();
+// });
+
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.left_nav');
+
+function mobileMenu() {
+  hamburger.classList.toggle('active');
+  navMenu.classList.toggle('active');
+}
+
+hamburger.addEventListener('click', mobileMenu);
